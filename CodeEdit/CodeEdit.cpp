dec091a086e3aea1863ef3abb29370717864b354
chenyfCode <1984008636@qq.com>
2022-12-22 11:38:08 +0800

    校正SIFT提取测距 特征点标识.txt  标定SIFT特征提取三角测距计算距离ransac。txt做较大修改  距离计算部分选用的容器有问题 应使用ransac的容器，但原来使用的是min

--- CodeEdit/CodeEdit.cpp
+++ CodeEdit/CodeEdit.cpp
@@ -10,39 +10,32 @@
 using namespace cv;
 using namespace std;
 
-//***************************参数以及初始化******************************//
-//Matlab和OpenCV进行相机标定得到的参数矩阵全是互为转置的!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 const char* imageName_L = "../Images/1206-8mm-2602mm/A0.png"; // 用于检测深度的图像
 const char* imageName_R = "../Images/1206-8mm-2602mm/B0.png";
-const char* stereoRectifyParams = "./image/stereoRectifyParams.txt"; // 存放立体校正参数
+const char* imageList_L = "../Images/1206-8mm-2602mm/steroCalibDataL.txt"; // 左相机的标定图片名称列表
+const char* imageList_R = "../Images/1206-8mm-2602mm/steroCalibDataR.txt"; // 右相机的标定图片名称列表
 
-Mat cameraMatrix_L = (cv::Mat_<float>(3, 3) << 1758.77667720632, 0.279226745063228, 625.625946148216,
-	0, 1757.99844199530, 507.107936794840,
-	0, 0, 1); // 左相机的内参数
-Mat cameraMatrix_R = (cv::Mat_<float>(3, 3) << 1755.14940547475, 0.501649912236977, 631.214991270194,
-	0, 1754.27796470277, 482.534096146445,
-	0, 0, 1); // 右相机的内参数
-//左右相机畸变系数 最后一个参数置0的话效果不一样
-Mat distCoeffs_L = (Mat_<float>(1, 5) << -0.0934954931685911, 0.326306912697088, 3.45466701643577e-05, -0.000318033085229932, -0.525344303444370); // 左相机的畸变系数
-Mat distCoeffs_R = (Mat_<float>(1, 5) << -0.0848605945510993, 0.212305665934113, -0.000585145907633591, -0.000718899576042846, -0.0908279577541819);// 右相机的畸变系数
-//旋转矩阵
-Mat R = (cv::Mat_<double>(3, 3) << 0.998950576820749, -0.0214417692637061, 0.0404361389225034,
-	0.0202157170894231, 0.999330628013878, 0.0304896817948001,
-	-0.0410628248258496, -0.0296402841455635, 0.998716825718428);
-
-//平移矩阵
-Mat T = (cv::Mat_<double>(3, 1) << -181.536578507112,
-	0.126450133817349,
-	4.80301053133881);
-// 图像尺寸
-Size imageSize(1280, 1024);
-
+const char* singleCalibrate_result_L = "calibrationresults_L.txt"; // 存放左相机的标定结果
+const char* singleCalibrate_result_R = "calibrationresults_R.txt"; // 存放右相机的标定结果
+const char* stereoCalibrate_result_L = "stereocalibrateresult.txt"; // 存放立体标定结果
+const char* stereoRectifyParams = "stereoRectifyParams.txt"; // 存放立体校正参数
+vector<vector<Point2f>> corners_seq_L; // 所有角点坐标
+vector<vector<Point2f>> corners_seq_R;
+vector<vector<Point3f>> objectPoints_L; // 三维坐标
+vector<vector<Point3f>> objectPoints_R;
+Mat cameraMatrix_L = Mat(3, 3, CV_32FC1, Scalar::all(0)); // 相机的内参数
+Mat cameraMatrix_R = Mat(3, 3, CV_32FC1, Scalar::all(0)); // 初始化相机的内参数
+Mat distCoeffs_L = Mat(1, 5, CV_32FC1, Scalar::all(0)); // 相机的畸变系数
+Mat distCoeffs_R = Mat(1, 5, CV_32FC1, Scalar::all(0)); // 初始化相机的畸变系数
+Mat R, T, E, F; // 立体标定参数
 Mat R1, R2, P1, P2, Q; // 立体校正参数
 Mat mapl1, mapl2, mapr1, mapr2; // 图像重投影映射表
 Mat img1_rectified, img2_rectified, result3DImage; // 校正图像  深度图
+Size patternSize = Size(11, 8); // 行列内角点个数
+Size chessboardSize = Size(30, 30); // 棋盘上每个棋盘格的大小mm
+Size imageSize; // 图像尺寸
 Rect validRoi[2];
-
 
 //计算汉明距离
 void match_min(vector<DMatch> matches, vector<DMatch>& good_matches)
@@ -85,7 +78,151 @@ void ransac(vector<DMatch> matches, vector<KeyPoint> queryKeyPoint, vector<KeyPo
 			matches_ransac.push_back(matches[i]);
 }
 
+/*
+单目标定
+参数：
+	imageList		存放标定图片名称的txt
+	singleCalibrateResult	存放标定结果的txt
+	objectPoints	世界坐标系中点的坐标
+	corners_seq		存放图像中的角点,用于立体标定
+	cameraMatrix	相机的内参数矩阵
+	distCoeffs		相机的畸变系数
+	imageSize		输入图像的尺寸（像素）
+	patternSize		标定板每行的角点个数, 标定板每列的角点个数
+	chessboardSize	棋盘上每个方格的边长（mm）
+注意：亚像素精确化时，允许输入单通道，8位或者浮点型图像。由于输入图像的类型不同，下面用作标定函数参数的内参数矩阵和畸变系数矩阵在初始化时也要数据注意类型。
+*/
+bool singleCameraCalibrate(const char* imageList, const char* singleCalibrateResult, vector<vector<Point3f>>& objectPoints,
+	vector<vector<Point2f>>& corners_seq, Mat& cameraMatrix, Mat& distCoeffs, Size& imageSize, Size patternSize, Size chessboardSize)
+{
+	int n_boards = 0;
+	ifstream imageStore(imageList); // 打开存放标定图片名称的txt
+	ofstream resultStore(singleCalibrateResult); // 保存标定结果的txt
+	// 开始提取角点坐标
+	vector<Point2f> corners; // 存放一张图片的角点坐标 
+	string imageName; // 读取的标定图片的名称
+	while (getline(imageStore, imageName)) // 读取txt的每一行（每一行存放了一张标定图片的名称）
+	{
+		n_boards++;
+		Mat imageInput = imread(imageName);
+		cvtColor(imageInput, imageInput, CV_RGB2GRAY);
+		imageSize.width = imageInput.cols; // 获取图片的宽度
+		imageSize.height = imageInput.rows; // 获取图片的高度
+		// 查找标定板的角点
+		bool found = findChessboardCorners(imageInput, patternSize, corners); // 最后一个参数int flags的缺省值为：CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE
+		// 亚像素精确化。在findChessboardCorners中自动调用了cornerSubPix，为了更加精细化，我们自己再调用一次。
+		if (found) // 当所有的角点都被找到
+		{
+			TermCriteria criteria = TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 40, 0.001); // 终止标准，迭代40次或者达到0.001的像素精度
+			cornerSubPix(imageInput, corners, Size(11, 11), Size(-1, -1), criteria);// 由于我们的图像只存较大，将搜索窗口调大一些，（11， 11）为真实窗口的一半，真实大小为（11*2+1， 11*2+1）--（23， 23）
+			corners_seq.push_back(corners); // 存入角点序列
+			// 绘制角点
+			//drawChessboardCorners(imageInput, patternSize, corners, true);
+			//imshow("cornersframe", imageInput);
+			//waitKey(500); // 暂停0.5s
+		}
+	}
+	//destroyWindow("cornersframe");
+	// 进行相机标定
+	// 计算角点对应的三维坐标
+	int pic, i, j;
+	for (pic = 0; pic < n_boards; pic++)
+	{
+		vector<Point3f> realPointSet;
+		for (i = 0; i < patternSize.height; i++)
+		{
+			for (j = 0; j < patternSize.width; j++)
+			{
+				Point3f realPoint;
+				// 假设标定板位于世界坐标系Z=0的平面
+				realPoint.x = j * chessboardSize.width;
+				realPoint.y = i * chessboardSize.height;
+				realPoint.z = 0;
+				realPointSet.push_back(realPoint);
+			}
+		}
+		objectPoints.push_back(realPointSet);
+	}
+	// 执行标定程序
+	vector<Mat> rvec; // 旋转向量
+	vector<Mat> tvec; // 平移向量
+	calibrateCamera(objectPoints, corners_seq, imageSize, cameraMatrix, distCoeffs, rvec, tvec, 0);
+	// 保存标定结果
+	resultStore << "相机内参数矩阵" << endl;
+	resultStore << cameraMatrix << endl << endl;
+	resultStore << "相机畸变系数" << endl;
+	resultStore << distCoeffs << endl << endl;
+	// 计算重投影点，与原图角点比较，得到误差
+	double errPerImage = 0.; // 每张图像的误差
+	double errAverage = 0.; // 所有图像的平均误差
+	double totalErr = 0.; // 误差总和
+	vector<Point2f> projectImagePoints; // 重投影点
+	for (i = 0; i < n_boards; i++)
+	{
+		vector<Point3f> tempObjectPoints = objectPoints[i]; // 临时三维点
+		// 计算重投影点
+		projectPoints(tempObjectPoints, rvec[i], tvec[i], cameraMatrix, distCoeffs, projectImagePoints);
+		// 计算新的投影点与旧的投影点之间的误差
+		vector<Point2f> tempCornersPoints = corners_seq[i];// 临时存放旧投影点
+		Mat tempCornersPointsMat = Mat(1, tempCornersPoints.size(), CV_32FC2); // 定义成两个通道的Mat是为了计算误差
+		Mat projectImagePointsMat = Mat(1, projectImagePoints.size(), CV_32FC2);
+		// 赋值
+		for (int j = 0; j < tempCornersPoints.size(); j++)
+		{
+			projectImagePointsMat.at<Vec2f>(0, j) = Vec2f(projectImagePoints[j].x, projectImagePoints[j].y);
+			tempCornersPointsMat.at<Vec2f>(0, j) = Vec2f(tempCornersPoints[j].x, tempCornersPoints[j].y);
+		}
+		// opencv里的norm函数其实把这里的两个通道分别分开来计算的(X1-X2)^2的值，然后统一求和，最后进行根号
+		errPerImage = norm(tempCornersPointsMat, projectImagePointsMat, NORM_L2) / (patternSize.width * patternSize.height);
+		totalErr += errPerImage;
+		resultStore << "第" << i + 1 << "张图像的平均误差为：" << errPerImage << endl;
+	}
+	resultStore << "全局平局误差为：" << totalErr / n_boards << endl;
+	imageStore.close();
+	resultStore.close();
+	return true;
+}
 
+/*
+双目标定:计算两摄像机相对旋转矩阵 R,平移向量 T, 本征矩阵E, 基础矩阵F
+参数：
+	stereoCalibrateResult	存放立体标定结果的txt
+	objectPoints			三维点
+	imagePoints				二维图像上的点
+	cameraMatrix			相机内参数
+	distCoeffs				相机畸变系数
+	imageSize				图像尺寸
+	R		左右相机相对的旋转矩阵
+	T		左右相机相对的平移向量
+	E		本征矩阵
+	F		基础矩阵
+*/
+bool stereoCalibrate(const char* stereoCalibrateResult, vector<vector<Point3f>> objectPoints, vector<vector<Point2f>> imagePoints1, vector<vector<Point2f>> imagePoints2,
+	Mat& cameraMatrix1, Mat& distCoeffs1, Mat& cameraMatrix2, Mat& distCoeffs2, Size& imageSize, Mat& R, Mat& T, Mat& E, Mat& F)
+{
+	ofstream stereoStore(stereoCalibrateResult);
+	TermCriteria criteria = TermCriteria(TermCriteria::COUNT | TermCriteria::EPS, 30, 1e-6); // 终止条件
+	stereoCalibrate(objectPoints, imagePoints1, imagePoints2, cameraMatrix1, distCoeffs1,
+		cameraMatrix2, distCoeffs2, imageSize, R, T, E, F, CALIB_FIX_INTRINSIC, criteria); // 注意参数顺序，可以到保存的文件中查看，避免返回时出错
+	stereoStore << "左相机内参数：" << endl;
+	stereoStore << cameraMatrix1 << endl;
+	stereoStore << "右相机内参数：" << endl;
+	stereoStore << cameraMatrix2 << endl;
+	stereoStore << "左相机畸变系数：" << endl;
+	stereoStore << distCoeffs1 << endl;
+	stereoStore << "右相机畸变系数：" << endl;
+	stereoStore << distCoeffs2 << endl;
+	stereoStore << "旋转矩阵：" << endl;
+	stereoStore << R << endl;
+	stereoStore << "平移向量：" << endl;
+	stereoStore << T << endl;
+	stereoStore << "本征矩阵：" << endl;
+	stereoStore << E << endl;
+	stereoStore << "基础矩阵：" << endl;
+	stereoStore << F << endl;
+	stereoStore.close();
+	return true;
+}
 
 /*
 立体校正
@@ -163,18 +300,18 @@ bool computeDisparityImage(const char* imageName1, const char* imageName2, Mat& 
 	Mat canRight = canvas(Rect(imageSize.width, 0, imageSize.width, imageSize.height));
 	gray_img1.copyTo(canLeft);
 	gray_img2.copyTo(canRight);
-	imwrite("./image/校正前左右相机图像.jpg", canvas);
+	imwrite("校正前左右相机图像.jpg", canvas);
 	remap(gray_img1, img1_rectified, mapl1, mapl2, INTER_LINEAR);
 	remap(gray_img2, img2_rectified, mapr1, mapr2, INTER_LINEAR);
-	imwrite("./image/左相机校正图像.jpg", img1_rectified);
-	imwrite("./image/右相机校正图像.jpg", img2_rectified);
+	imwrite("左相机校正图像.jpg", img1_rectified);
+	imwrite("右相机校正图像.jpg", img2_rectified);
 	img1_rectified.copyTo(canLeft);
 	img2_rectified.copyTo(canRight);
 	rectangle(canLeft, validRoi[0], Scalar(255, 255, 255), 5, 8);
 	rectangle(canRight, validRoi[1], Scalar(255, 255, 255), 5, 8);
 	for (int j = 0; j <= canvas.rows; j += 16)
 		line(canvas, Point(0, j), Point(canvas.cols, j), Scalar(0, 255, 0), 1, 8);
-	imwrite("./image/校正后左右相机图像.jpg", canvas);
+	imwrite("校正后左右相机图像.jpg", canvas);
 
 
 	// 进行SIFT特征点提取与匹配
@@ -211,7 +348,7 @@ bool computeDisparityImage(const char* imageName1, const char* imageName2, Mat& 
 	cv::Mat img_matches_bf;
 	drawMatches(img1_rectified, keypoints1, img2_rectified, keypoints2, matches, img_matches_bf);
 	imshow("暴力匹配", img_matches_bf);
-	imwrite("./image/暴力匹配.jpg", img_matches_bf);
+
 	vector<DMatch>  good_min, good_ransac;
 	//最小汉明距离筛选暴力匹配结果
 	match_min(matches, good_min);
@@ -219,35 +356,30 @@ bool computeDisparityImage(const char* imageName1, const char* imageName2, Mat& 
 	//用ransac算法筛选暴力匹配结果
 	ransac(good_min, keypoints1, keypoints2, good_ransac);
 	cout << "good_matches.size=" << good_ransac.size() << endl;
-	
 	Mat  outimg;
-	drawMatches(img1_rectified, keypoints1, img2_rectified, keypoints2, good_ransac, outimg, 
-		        cv::Scalar::all(-1), cv::Scalar::all(-1), vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-	
+	drawMatches(img1_rectified, keypoints1, img2_rectified, keypoints2, good_ransac, outimg);
+	imshow("ransac算法筛选暴力匹配结果", outimg);
 
 	/********相似三角形测距法 计算距离*******/
 	double B = sqrt(T.at<double>(0, 0) * T.at<double>(0, 0) + T.at<double>(1, 0) * T.at<double>(1, 0) + T.at<double>(2, 0) * T.at<double>(2, 0));
 	double f = Q.at<double>(2, 3);
 	double avr_dis = 0;
 	double max_dis = LONG_MIN, min_dis = LONG_MAX;
-	Point origin;
 	cout << "f = " << f << endl;
 	cout << "B = " << B << endl;
 	cout << "********************************************" << endl;
-	for (int i = 0; i < good_min.size(); i++)
+	for (int i = 0; i < good_ransac.size(); i++)
 	{
-		double x_L = keypoints1[good_min[i].queryIdx].pt.x;
-		double x_R = keypoints2[good_min[i].trainIdx].pt.x;
-		origin.x = keypoints1[good_min[i].queryIdx].pt.x;
-		origin.y = keypoints1[good_min[i].queryIdx].pt.y;
+		double x_L = keypoints1[good_ransac[i].queryIdx].pt.x;
+		double x_R = keypoints2[good_ransac[i].trainIdx].pt.x;
 
-		putText(outimg, to_string(good_min[i].queryIdx), origin, cv::FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
 		double distance = f * B / abs(x_L - x_R);
-		
-		cout << to_string(good_min[i].queryIdx)<< " 点的x坐标为: " << x_L<<" 求得的距离为： "<< distance << " mm" << endl;
+		//cout << "x_L = " << x_L << endl;
+		//cout << "x_R = " << x_R << endl;
+		cout << "距离为: " << distance << " mm" << endl;
 	}
-	imshow("ransac算法筛选暴力匹配结果", outimg);
-	imwrite("./image/ransac算法筛选暴力匹配结果.jpg", outimg);
+
+
 	return true;
 }
 
@@ -265,6 +397,15 @@ void onMouse(int event, int x, int y, int flags, void* param)
 
 int main()
 {
+	singleCameraCalibrate(imageList_L, singleCalibrate_result_L, objectPoints_L, corners_seq_L, cameraMatrix_L,
+		distCoeffs_L, imageSize, patternSize, chessboardSize);
+	cout << "已完成左相机的标定!" << endl;
+	singleCameraCalibrate(imageList_R, singleCalibrate_result_R, objectPoints_R, corners_seq_R, cameraMatrix_R,
+		distCoeffs_R, imageSize, patternSize, chessboardSize);
+	cout << "已完成右相机的标定!" << endl;
+	stereoCalibrate(stereoCalibrate_result_L, objectPoints_L, corners_seq_L, corners_seq_R, cameraMatrix_L, distCoeffs_L,
+		cameraMatrix_R, distCoeffs_R, imageSize, R, T, E, F);
+	cout << "相机立体标定完成！" << endl;
 	//进行立体校正
 	validRoi[0], validRoi[1] = stereoRectification(stereoRectifyParams, cameraMatrix_L, distCoeffs_L, cameraMatrix_R, distCoeffs_R,
 		imageSize, R, T, R1, R2, P1, P2, Q, mapl1, mapl2, mapr1, mapr2);
